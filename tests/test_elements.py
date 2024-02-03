7f776ce77fdbcf1ba1dcbab51e4ed4c2f891715a
Elizaveta Frolova <elizaveta.fr@gmail.com>
2024-02-03 18:56:15 +0300

    Update

--- tests/test_elements.py
+++ tests/test_elements.py
@@ -1,9 +1,27 @@
 import time
 
-from pages.base_page import BasePage
+# from pages.base_page import BasePage
+#
+#
+# def test(driver):
+#     page = BasePage(driver, 'https://google.com')
+#     page.open()
+#     time.sleep(5)
+#
+
+from pages.elements_page import TextBoxPage
 
 
-def test(driver):
-    page = BasePage(driver, 'https://google.com')
-    page.open()
-    time.sleep(5)
+class TestElements:
+    class TestTextBox:
+
+        def test_text_box(self, driver):
+            text_box_page = TextBoxPage(driver, 'https://demoqa.com/text-box')
+            text_box_page.open()
+            #  driver.execute_script("window.scrollTo(0, 200)") - чтобы кнопка submit была видна
+            text_box_page.fill_all_fields()
+            output_name, output_email, output_curr_address, output_perm_address = text_box_page.check_filled_form()
+            print(output_name)
+            print(output_email)
+            print(output_curr_address)
+            print(output_perm_address)
