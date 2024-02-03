15e5d9913f4d06e1588b466a68a6aea799229dc1
Elizaveta Frolova <elizaveta.fr@gmail.com>
2024-02-03 13:41:54 +0300

    Update

--- tests/test_elements.py
+++ tests/test_elements.py
@@ -6,4 +6,4 @@ from pages.base_page import BasePage
 def test(driver):
     page = BasePage(driver, 'https://google.com')
     page.open()
-    time.sleep(5)  # это пишем, чтобы увидеть, как браузер открывается данную страницу
+    time.sleep(5)
